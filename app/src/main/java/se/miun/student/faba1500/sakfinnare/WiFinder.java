cda6b059b0c7d48b48c06fcb746962c228e0fbab
Fabian Bakkum <fabianbakkum@hotmail.com>
2018-04-19 23:22:56 +0200

    Working GET Request

--- app/src/main/java/se/miun/student/faba1500/sakfinnare/WiFinder.java
+++ app/src/main/java/se/miun/student/faba1500/sakfinnare/WiFinder.java
@@ -6,11 +6,19 @@ import android.arch.persistence.room.PrimaryKey;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 
 @Entity
 public class WiFinder {
@@ -48,12 +56,61 @@ public class WiFinder {
         return key;
     }
 
+    public class WifiReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager cm =
+                    (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            if(activeNetwork != null && activeNetwork.isConnected()) {
+                Toast connectedToast = Toast.makeText(
+                        context,
+                        "Connected...",
+                        Toast.LENGTH_LONG
+                );
+
+                connectedToast.show();
+
+                try {
+                    new SendGetRequestTask().execute(new URL("http://10.42.0.1:8080/status/"));
+                } catch(MalformedURLException e) {}
+
+                context.unregisterReceiver(this);
+            }
+        }
+    }
+
     public int setState(boolean lost, WeakReference<Context> context) {
         if(lost) {
             WifiConfiguration configuration = new WifiConfiguration();
             configuration.SSID = "Sakfinnare";
             configuration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
 
+            WifiManager manager = (WifiManager) context.get().getApplicationContext()
+                    .getSystemService(Context.WIFI_SERVICE);
+
+            // Remove all old configurations first, in case the user changed it.
+            // Yes, this is safe, because we only have permission to delete configurations
+            // that are created by this application.
+            for(WifiConfiguration i : Objects.requireNonNull(manager).getConfiguredNetworks()) {
+                manager.removeNetwork(i.networkId);
+            }
+
+            // Enable WiFi in case its disabled.
+            manager.setWifiEnabled(true);
+
+            WifiReceiver receiver = new WifiReceiver();
+            context.get().registerReceiver(receiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
+            // Connect to the network.
+            manager.enableNetwork(manager.addNetwork(configuration), true);
+
+
+
+            // OLD:
+
+            /*
             ConnectToNetworkTask task = new ConnectToNetworkTask(context, configuration);
             ConnectToNetworkTask.PostExecuteListener postExecuteListener = new ConnectToNetworkTask.PostExecuteListener() {
                 @Override
@@ -73,6 +130,7 @@ public class WiFinder {
 
             task.setPostExecuteListener(postExecuteListener);
             task.execute();
+            */
         }
 
         return 0;
